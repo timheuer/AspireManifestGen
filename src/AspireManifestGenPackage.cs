23de994899e2d66a77d581610dee5d22af454f1d
Tim Heuer <tim@timheuer.com>
2023-12-01 15:23:19 -0800

    Fix #2 by changing command to DynamicVisibility in VSCT

--- src/AspireManifestGenPackage.cs
+++ src/AspireManifestGenPackage.cs
@@ -12,9 +12,16 @@ namespace AspireManifestGen;
 [Guid(PackageGuids.AspireManifestGenString)]
 [ProvideOptionPage(typeof(OptionsProvider.GeneralOptions), ".NET Aspire", "Manifest", 0, 0, true)]
 [ProvideProfile(typeof(OptionsProvider.GeneralOptions), ".NET Aspire", "Manifest", 0, 0, true)]
-
+[ProvideAutoLoad(UIContextGuid)]
+[ProvideUIContextRule(UIContextGuid,
+    name: "Show on Aspire Project",
+    expression: "Aspire",
+    termNames: ["Aspire"],
+    termValues: ["ActiveProjectCapability:Aspire"])]
 public sealed class AspireManifestGenPackage : ToolkitPackage
 {
+    public const string UIContextGuid = "F686D1D0-9DDF-47DB-A0DC-59032E168F69";
+
     protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
     {
         await this.RegisterCommandsAsync();
